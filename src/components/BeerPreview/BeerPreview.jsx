458a30fffcc8b5900339691c233a61b598258bbf
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-07-27 19:10:22 +0300

    some

--- src/components/BeerPreview/BeerPreview.jsx
+++ src/components/BeerPreview/BeerPreview.jsx
@@ -4,7 +4,7 @@ import style from "./BeerPreview.module.scss";
 export const BeerPreview = ({ children }) => {
   return (
     <section className={style.section}>
-      <h1>Наше пиво</h1>
+      <h1 className={style.title}>Наше пиво</h1>
       <div className={style.beeritems}>{children}</div>
     </section>
   );
