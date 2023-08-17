c1800014597fff9badfb96ea2d5604e737049537
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-17 20:56:11 +0300

    animation

--- src/components/RunningLline/RunningLline.jsx
+++ src/components/RunningLline/RunningLline.jsx
@@ -6,9 +6,17 @@ export const RunningLline = () => {
   return (
     <section className={style.section}>
       <BrokenPaper />
-      <p className={style.runningLline}>
-        пиво - бургери - снеки - фритюр - пиво - бургери - снеки - фритюр
-      </p>
+      <div className={style.runningLlineBlock}>
+        <p className={style.runningLlineText}>
+          пиво - бургери - снеки - фритюр - пиво - бургери - снеки - фритюр -
+          пиво - бургери - снеки - фритюр - пиво - бургери - снеки - фритюр -
+          пиво - бургери - снеки - фритюр - пиво - бургери - снеки - фритюр -
+          пиво - бургери - снеки - фритюр - пиво - бургери - снеки - фритюр -
+          пиво - бургери - снеки - фритюр - пиво - бургери - снеки - фритюр -
+          пиво - бургери - снеки - фритюр - пиво - бургери - снеки - фритюр -
+        </p>
+      </div>
+
       <div className={style.lineRotate}>
         <BrokenPaper />
       </div>
