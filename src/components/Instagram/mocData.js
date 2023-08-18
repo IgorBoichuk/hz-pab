0396505f76347ed5992b6380e5536bb97f4a9a53
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-18 21:03:13 +0300

    Create mocData.js

---
+++ src/components/Instagram/mocData.js
@@ -0,0 +1,42 @@
+const instaData = [
+  {
+    image:
+      "https://www.geneva.te.ua/wp-content/uploads/2020/02/pexels-photo-3171837-1024x684.jpeg",
+    title: "#крафтовепиво",
+  },
+  {
+    image:
+      "https://www.geneva.te.ua/wp-content/uploads/2020/02/pexels-photo-3171837-1024x684.jpeg",
+    title: "#halloween",
+  },
+  {
+    image:
+      "https://www.geneva.te.ua/wp-content/uploads/2020/02/pexels-photo-3171837-1024x684.jpeg",
+    title: "#смаколики",
+  },
+  {
+    image:
+      "https://www.geneva.te.ua/wp-content/uploads/2020/02/pexels-photo-3171837-1024x684.jpeg",
+    title: "#келихігристого",
+  },
+  {
+    image:
+      "https://www.geneva.te.ua/wp-content/uploads/2020/02/pexels-photo-3171837-1024x684.jpeg",
+    title: "#party",
+  },
+  {
+    image:
+      "https://www.geneva.te.ua/wp-content/uploads/2020/02/pexels-photo-3171837-1024x684.jpeg",
+    title: "#some-else",
+  },
+  {
+    image:
+      "https://www.geneva.te.ua/wp-content/uploads/2020/02/pexels-photo-3171837-1024x684.jpeg",
+    title: "#and-again",
+  },
+  {
+    image:
+      "https://www.geneva.te.ua/wp-content/uploads/2020/02/pexels-photo-3171837-1024x684.jpeg",
+    title: "#superstar",
+  },
+];
