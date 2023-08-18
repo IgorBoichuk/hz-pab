90615fb78a62dd1e43049119ad7e539d139e4bb9
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-18 23:00:33 +0300

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,54 @@
 import React from "react";
 
 import style from "./footer.module.scss";
+import { Container } from "../Container/Container";
+import { Link } from "react-router-dom";
+import { Navmenu } from "../Navmenu/Navmenu";
+import Logo from "../../assets/galary/hzfav.png";
+import srite from "../../assets/galary/svg/sprite.svg";
 
 export const Footer = () => {
   return (
     <footer className={style.footer}>
-      <p className={style.footercontent}>footer</p>
+      <Container>
+        <div className={style.footerWrapper}>
+          <Link to="/" className={style.logolink}>
+            <img src={Logo} alt="Logo" className={style.logo} />
+          </Link>
+          <nav className={style.navmenu}>
+            <Navmenu />
+          </nav>
+          <div className={style.timetWrapper}>
+            <p className={style.contactTitle}>Години роботи:</p>
+            <p className={style.contact}>
+              пн-пт 16:00 - 22:00 сб-нд 16:00 - 18:00
+            </p>
+          </div>
+          <div className={style.adressWrapper}>
+            <p className={style.contactTitle}>Адреса:</p>
+            <p className={style.contact}>
+              вулиця Соборна, 107, Софіївська Борщагівка, Київ
+            </p>
+          </div>
+          <div>
+            <div className={style.sicialWrapper}>
+              <Link to="https://www.facebook.com/">
+                <svg className={style.sicialItem} width="40" height="40">
+                  <use xlinkHref={`${srite}#facebook`}></use>
+                </svg>
+              </Link>
+              <Link to="https://www.instagram.com/craftbeer_pub_2020/">
+                <svg className={style.sicialItem} width="40" height="40">
+                  <use xlinkHref={`${srite}#instagram`}></use>
+                </svg>
+              </Link>
+            </div>
+            <Link to="tel:+380662408540" className={style.navlink}>
+              +380662408540
+            </Link>
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 };
