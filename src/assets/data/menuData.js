72873d0d3a3efc87b9ab716729e69f7f8a3505c1
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-18 23:33:20 +0300

    some data

--- src/assets/data/menuData.js
+++ src/assets/data/menuData.js
@@ -147,7 +147,7 @@ export const menu = [
     category: "fries",
     name: "Картопля фрі",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://www.uman.prontopizza.ua/wp-content/uploads/2018/01/potato-100.jpg",
     weight: "150 грам",
     price: 60,
   },
@@ -155,23 +155,21 @@ export const menu = [
     category: "fries",
     name: "Картопляні діпи",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://pablito.com.ua/wp-content/uploads/2020/09/kartofelnie-dipi-pablito.jpg",
     weight: "150 грам",
     price: 69,
   },
   {
     category: "fries",
     name: "Нагетси",
-    image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+    image: "https://today.ua/wp-content/uploads/2022/09/naggetsy.jpg",
     weight: "6 шт",
     price: 79,
   },
   {
     category: "fries",
     name: "Нагетси",
-    image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+    image: "https://today.ua/wp-content/uploads/2022/09/naggetsy.jpg",
     weight: "9 шт",
     price: 109,
   },
@@ -179,7 +177,7 @@ export const menu = [
     category: "fries",
     name: "Моцарела фрі",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://i.lefood.menu/wp-content/uploads/w_images/2022/02/recept-32606-1240x827.jpg",
     weight: "5 шт",
     price: 129,
   },
@@ -187,7 +185,7 @@ export const menu = [
     category: "fries",
     name: "Сирні палички",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://shuba.life/static/content/thumbs/740x493/0/e2/nycune---c740x493x50px50p-c740x493x50px50p-up--b06d513ad8a009b64adbc6cafe4fde20.jpg",
     weight: "5 шт",
     price: 69,
   },
@@ -195,7 +193,7 @@ export const menu = [
     category: "fries",
     name: "Сирні палички",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://shuba.life/static/content/thumbs/740x493/0/e2/nycune---c740x493x50px50p-c740x493x50px50p-up--b06d513ad8a009b64adbc6cafe4fde20.jpg",
     weight: "10 шт",
     price: 129,
   },
@@ -203,7 +201,7 @@ export const menu = [
     category: "fries",
     name: "Попкорн",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://www.rbc.ua/static/img/k/_/k_photo_recipe_ramp_up_2022_02_cheese_popcorn_cheese_popcorn_2_1300x820.jpg",
     weight: "100 грам",
     price: 49,
   },
@@ -211,7 +209,7 @@ export const menu = [
     category: "fries",
     name: "Попкорн",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://www.rbc.ua/static/img/k/_/k_photo_recipe_ramp_up_2022_02_cheese_popcorn_cheese_popcorn_2_1300x820.jpg",
     weight: "200 грам",
     price: 89,
   },
@@ -219,23 +217,21 @@ export const menu = [
     category: "fries",
     name: "Креветки панко",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://media-cdn.tripadvisor.com/media/photo-s/1c/d3/98/81/sweet-chili.jpg",
     weight: "5 шт",
     price: 79,
   },
   {
     category: "fries",
     name: "Стріпси",
-    image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+    image: "https://sushi7.com.ua/wp-content/uploads/2022/05/stripsy.jpg",
     weight: "5 шт",
     price: 112,
   },
   {
     category: "fries",
     name: "Стріпси",
-    image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+    image: "https://sushi7.com.ua/wp-content/uploads/2022/05/stripsy.jpg",
     weight: "10 шт",
     price: 209,
   },
@@ -243,7 +239,7 @@ export const menu = [
     category: "fries",
     name: "Креветки королівські",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://s0.tchkcdn.com/i/13/1/345046_3e2a86fe_5d6d12da80b19e07cff735e5c90f33f0_04_kak_varit_zamorozhennye_krevetki_depositphotos_22575483_m.jpg",
     weight: "5 шт",
     price: 169,
   },
@@ -251,7 +247,7 @@ export const menu = [
     category: "fries",
     name: "Цибулеві кільця",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://shuba.life/static/content/thumbs/740x493/d/98/jfwvsb---c740x493x50px50p-c740x493x50px50p-up--e6c7028bd7d2476b6800c57965bbc98d.jpg",
     weight: "6 шт",
     price: 69,
   },
@@ -259,7 +255,7 @@ export const menu = [
     category: "fries",
     name: "Цибулеві кільця",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://shuba.life/static/content/thumbs/740x493/d/98/jfwvsb---c740x493x50px50p-c740x493x50px50p-up--e6c7028bd7d2476b6800c57965bbc98d.jpg",
     weight: "9 шт",
     price: 89,
   },
@@ -267,7 +263,7 @@ export const menu = [
     category: "fries",
     name: "Крильця",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://images.1plus1.video/news-1/34555/f21950704c54e3e8036d32ab9ebc4b00.1020x565.jpg",
     weight: "5 шт",
     price: 129,
   },
@@ -275,23 +271,21 @@ export const menu = [
     category: "fries",
     name: "Крильця",
     image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+      "https://images.1plus1.video/news-1/34555/f21950704c54e3e8036d32ab9ebc4b00.1020x565.jpg",
     weight: "10 шт",
     price: 229,
   },
   {
     category: "fries",
     name: "Кульки філадельфія",
-    image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+    image: "https://kartofan.ua/wp-content/uploads/2020/01/2-2-700x470.jpg",
     weight: "6 шт",
     price: 69,
   },
   {
     category: "fries",
     name: "Кульки філадельфія",
-    image:
-      "https://luxor-ukraine.com.ua/wp-content/uploads/2021/10/2361541520_w600_h600_2361541520.jpg",
+    image: "https://kartofan.ua/wp-content/uploads/2020/01/2-2-700x470.jpg",
     weight: "10 шт",
     price: 109,
   },
