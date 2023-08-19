087bec9f299d9046a209da2784b465c4d7dac53d
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-19 17:30:52 +0300

    ContactsCardsList

--- src/components/ContactsCardsList/ContactsCardsList.jsx
+++ src/components/ContactsCardsList/ContactsCardsList.jsx
@@ -1,6 +1,63 @@
 import React from "react";
 import style from "./ContactsCardsList.module.scss";
+import { Container } from "../Container/Container";
+import { Link } from "react-router-dom";
+import srite from "../../assets/galary/svg/sprite.svg";
 
 export const ContactsCardsList = () => {
-  return <section className={style.section}>ContactsCardsList</section>;
+  const social = (
+    <div className={style.sicialWrapper}>
+      <Link to="https://www.facebook.com/HzPubCraftbeer">
+        <svg className={style.sicialItem} width="40" height="40">
+          <use xlinkHref={`${srite}#facebook`}></use>
+        </svg>
+      </Link>
+      <Link to="https://www.instagram.com/craftbeer_pub_2020/">
+        <svg className={style.sicialItem} width="40" height="40">
+          <use xlinkHref={`${srite}#instagram`}></use>
+        </svg>
+      </Link>
+    </div>
+  );
+
+  const contactsInfo = [
+    {
+      title: "Години роботи:",
+      info: "пн-пт 16:00 - 22:00, сб-нд 16:00 - 18:00",
+    },
+    {
+      title: "Адреса: ",
+      info: "вулиця Соборна, 107, Софіївська Борщагівка, Київ",
+    },
+    { title: "НоМЕР ТЕЛЕФОНУ", info: "+380123456789" },
+    { title: "СОЦІАЛЬНІ МЕРЕЖІ", info: "" },
+  ];
+  return (
+    <section className={style.section}>
+      <Container>
+        <div className={style.cardsWrapper}>
+          <ul className={style.cardsList}>
+            {contactsInfo.map((item) => (
+              <li className={style.card}>
+                <p className={style.title}>{item.title}</p>
+                <p className={style.info}>{item.info || social}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Container>
+      {/* <div className={style.sicialWrapper}>
+        <Link to="https://www.facebook.com/HzPubCraftbeer">
+          <svg className={style.sicialItem} width="40" height="40">
+            <use xlinkHref={`${srite}#facebook`}></use>
+          </svg>
+        </Link>
+        <Link to="https://www.instagram.com/craftbeer_pub_2020/">
+          <svg className={style.sicialItem} width="40" height="40">
+            <use xlinkHref={`${srite}#instagram`}></use>
+          </svg>
+        </Link>
+      </div> */}
+    </section>
+  );
 };
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { MainSection } from "../components/MainSection/MainSection";
 import { GoogleMap } from "../components/GoogleMap/GoogleMap";
+import { ContactsCardsList } from "../components/ContactsCardsList/ContactsCardsList";
 
 export const Contacts = () => {
   return (
     <div>
       <MainSection title={"контакти"} />
+      <ContactsCardsList />
       <GoogleMap />
     </div>
   );
