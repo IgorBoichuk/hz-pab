9987fcb21ceab506588090f6e9c7fd7ebe2bd8b1
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-21 20:25:51 +0300

    some of AsideMenu

--- src/components/AsideMenu/AsideMenu.jsx
+++ src/components/AsideMenu/AsideMenu.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import style from "./AsideMenu.module.scss";
 import { Link } from "react-router-dom";
+import { Container } from "../Container/Container";
 
 export const AsideMenu = () => {
+  const sideMenu = [
+    {
+      menuItem: "Бар",
+      menuLink: "bar",
+    },
+    {
+      menuItem: "Снеки",
+      menuLink: "snacks",
+    },
+    {
+      menuItem: "Кухня",
+      menuLink: "kitchen",
+    },
+    {
+      menuItem: "Соуси",
+      menuLink: "souses",
+    },
+  ];
   return (
     <aside className={style.aside}>
       <ul className={style.list}>
-        <Link to="bar" className={style.linkItem}>
-          <li className={style.listItem}>Бар</li>
-        </Link>
-        <Link to="snacks" className={style.linkItem}>
-          <li className={style.listItem}>Снеки та закуски</li>
-        </Link>
-        <Link to="kitchen" className={style.linkItem}>
-          <li className={style.listItem}>Кухня</li>
-        </Link>
-        <Link to="souses" className={style.linkItem}>
-          <li className={style.listItem}>Соуси</li>
-        </Link>
+        {sideMenu.map((item) => (
+          <Link
+            to={item.menuLink}
+            className={style.linkItem}
+            key={item.menuItem}
+          >
+            <li className={style.listItem}>{item.menuItem}</li>
+          </Link>
+        ))}
       </ul>
     </aside>
   );
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-// import { BeerCard } from "../components/BeerCard/BeerCard";
-// import { beer } from "../components/data/beerData";
-// import { BeerPreview } from "../components/BeerPreview/BeerPreview";
+import { menu } from "../assets/data/menuData";
+import { BeerPreview } from "../components/BeerPreview/BeerPreview";
 import { AsideMenu } from "../components/AsideMenu/AsideMenu";
 import { Outlet } from "react-router-dom";
 import { MainSection } from "../components/MainSection/MainSection";
+import { ArticleCard } from "../components/ArticleCard/ArticleCard";
 
 export const Menu = () => {
   return (
@@ -13,11 +13,11 @@ export const Menu = () => {
       <MainSection title={"меню"} />
       <AsideMenu />
       <Outlet />
-      {/* <BeerPreview>
-        {beer.map((beer) => (
-          <BeerCard beer={beer} />
+      <BeerPreview>
+        {menu.map((item) => (
+          <ArticleCard item={item} />
         ))}
-      </BeerPreview> */}
+      </BeerPreview>
     </div>
   );
 };
