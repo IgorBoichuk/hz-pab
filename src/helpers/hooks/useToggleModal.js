1de6b14f18638e7e5c6d2ce10d70580ccf6a48fe
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-29 16:33:14 +0300

    in modal scroll off

--- src/helpers/hooks/useToggleModal.js
+++ src/helpers/hooks/useToggleModal.js
@@ -2,8 +2,17 @@ import { useState } from "react";
 
 export const useToggleModal = () => {
   const [isModal, setIsModal] = useState(false);
-  const open = () => setIsModal(true);
-  const close = () => setIsModal(false);
-  const toggle = () => setIsModal((isModal) => !isModal);
+  const open = () => {
+    setIsModal(true);
+    document.body.style.overflow = "hidden";
+  };
+  const close = () => {
+    setIsModal(false);
+    document.body.style.overflow = "auto";
+  };
+  const toggle = () => {
+    setIsModal((isModal) => !isModal);
+    document.body.style.overflow = !isModal ? "hidden" : "auto";
+  };
   return { isModal, open, close, toggle };
 };
