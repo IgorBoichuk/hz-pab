a44f10deb362948a04acf15b2579b5e10c1b6206
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-29 19:48:07 +0300

    BeerPreview

--- src/modules/Menu/BeerPreview/BeerPreview.jsx
+++ src/modules/Menu/BeerPreview/BeerPreview.jsx
@@ -1,13 +1,18 @@
 import React, { useContext } from "react";
 import style from "./BeerPreview.module.scss";
 import { MyContext } from "../../../Context/ContextProvider";
+import { ArticleCard } from "../../shared/ArticleCard/ArticleCard";
 
-export const BeerPreview = ({ children }) => {
-  const { preview } = useContext(MyContext);
+export const BeerPreview = () => {
+  const { preview, filteredData } = useContext(MyContext);
   return (
     <section className={style.section}>
       <h1 className={style.title}>{preview}</h1>
-      <ul className={style.beeritems}>{children}</ul>
+      <ul className={style.beeritems}>
+        {filteredData.map((item) => (
+          <ArticleCard item={item} key={item.id} />
+        ))}
+      </ul>
     </section>
   );
 };
--- src/modules/Menu/MenuContent/MenuContent.jsx
+++ src/modules/Menu/MenuContent/MenuContent.jsx
@@ -3,23 +3,14 @@ import style from "./MenuContent.module.scss";
 import { AsideMenu } from "../AsideMenu/AsideMenu";
 import { Container } from "../../shared/Container/Container";
 import { BeerPreview } from "../BeerPreview/BeerPreview";
-import { ArticleCard } from "../../shared/ArticleCard/ArticleCard";
-
-import { MyContext } from "../../../Context/ContextProvider";
 
 export const MenuContent = () => {
-  const { filteredData } = useContext(MyContext);
-
   return (
     <section className={style.section}>
       <Container>
         <div className={style.sectionWrapper}>
           <AsideMenu />
-          <BeerPreview>
-            {filteredData.map((item) => (
-              <ArticleCard item={item} key={item.id} />
-            ))}
-          </BeerPreview>
+          <BeerPreview />
         </div>
       </Container>
     </section>
