7fd4916588583dbacc407fc9c54334f896320c61
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-29 12:57:15 +0300

    footer

--- src/modules/About/TolkingAboutAs/TolkingAboutAs.jsx
+++ src/modules/About/TolkingAboutAs/TolkingAboutAs.jsx
@@ -7,30 +7,32 @@ export const TolkingAboutAs = ({ title }) => {
   return (
     <section className={style.section}>
       <Container>
-        <div className={style.sectionTitle}>
-          <p className={style.textOutline}>{title}</p>
-          <h3 className={style.title}>{title}</h3>
-        </div>
-        <div className={style.reviewsWrapper}>
-          <div className={style.reviews}>
-            <svg className={style.leftCorner} width="112" height="144">
-              <use xlinkHref={`${sprite}#corner`}></use>
-            </svg>
-            <p className={style.reviewsCount}>5/5</p>
-            <p className={style.reviewsPlace}>відгуки GOOGLE</p>
-            <svg className={style.rightCorner} width="112" height="144">
-              <use xlinkHref={`${sprite}#corner`}></use>
-            </svg>
+        <div className={style.sectionWrapper}>
+          <div className={style.sectionTitle}>
+            <p className={style.textOutline}>{title}</p>
+            <h3 className={style.title}>{title}</h3>
           </div>
-          <div className={style.reviews}>
-            <svg className={style.leftCorner} width="112" height="144">
-              <use xlinkHref={`${sprite}#corner`}></use>
-            </svg>
-            <p className={style.reviewsCount}>100+</p>
-            <p className={style.reviewsPlace}>відгуків в instagram</p>
-            <svg className={style.rightCorner} width="112" height="144">
-              <use xlinkHref={`${sprite}#corner`}></use>
-            </svg>
+          <div className={style.reviewsWrapper}>
+            <div className={style.reviews}>
+              <svg className={style.leftCorner} width="112" height="144">
+                <use xlinkHref={`${sprite}#corner`}></use>
+              </svg>
+              <p className={style.reviewsCount}>5/5</p>
+              <p className={style.reviewsPlace}>відгуки GOOGLE</p>
+              <svg className={style.rightCorner} width="112" height="144">
+                <use xlinkHref={`${sprite}#corner`}></use>
+              </svg>
+            </div>
+            <div className={style.reviews}>
+              <svg className={style.leftCorner} width="112" height="144">
+                <use xlinkHref={`${sprite}#corner`}></use>
+              </svg>
+              <p className={style.reviewsCount}>100+</p>
+              <p className={style.reviewsPlace}>відгуків в instagram</p>
+              <svg className={style.rightCorner} width="112" height="144">
+                <use xlinkHref={`${sprite}#corner`}></use>
+              </svg>
+            </div>
           </div>
         </div>
       </Container>
