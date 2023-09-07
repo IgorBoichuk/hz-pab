a43e0d53097e8d70fc51f5b67c2fd61fecfa036c
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-09-07 13:35:11 +0300

    beer images

--- src/assets/data/beerData.js
+++ src/assets/data/beerData.js
@@ -1,3 +1,8 @@
+import beerCup from "../galary/menu/beer/beer.webp";
+import seedr from "../galary/menu/beer/seedr.webp";
+import prosecco from "../galary/menu/beer/prosecco.webp";
+import kvas from "../galary/menu/beer/kvas.webp";
+
 export const beer = [
   {
     id: 100,
@@ -8,8 +13,7 @@ export const beer = [
     alcohol: 4.2,
     ibu: 12,
     weight: "0,5л",
-    image:
-      "https://i0.wp.com/samogonman.com/wp-content/uploads/2018/01/3633.jpg",
+    image: beerCup,
     description: "Пиво «Kölsch» має злегка гіркий смак, відчувається хміль",
     price: 49,
   },
@@ -22,8 +26,7 @@ export const beer = [
     alcohol: 4.5,
     ibu: 13,
     weight: "0,5л",
-    image:
-      "https://i0.wp.com/samogonman.com/wp-content/uploads/2018/01/3633.jpg",
+    image: beerCup,
     description:
       "Світле нефільтроване непастеризоване пиво «Пшеничне». Пиво має м’який солодовий смак із легкою залишковою гіркотою та пряним присмаком. Легкий фенольний аромат поєднується з нотками хмелю.",
     price: 54,
@@ -37,8 +40,7 @@ export const beer = [
     alcohol: 4.4,
     ibu: 15,
     weight: "0,5л",
-    image:
-      "https://i0.wp.com/samogonman.com/wp-content/uploads/2018/01/3633.jpg",
+    image: beerCup,
     description:
       "Світле нефільтроване непастеризоване пиво «Lager». Пиво має чистий солодовий смак з легкою гіркуватістю та тонким ароматом хмелю. Світло-солом’яний пінистий напій, який чудово втамовує спрагу.",
     price: 51,
@@ -52,8 +54,7 @@ export const beer = [
     alcohol: 5.6,
     ibu: 45,
     weight: "0,5л",
-    image:
-      "https://i0.wp.com/samogonman.com/wp-content/uploads/2018/01/3633.jpg",
+    image: beerCup,
     description:
       "Світле нефільтроване непастеризоване пиво «IPA», має легкий смак, солодовий з яскраво вираженою хмельовою гіркотою. Аромат яскравий насичений, квітково-цитрусовий, з нотами тропічних фруктів.",
     price: 61,
@@ -67,8 +68,7 @@ export const beer = [
     alcohol: 4.2,
     ibu: 12,
     weight: "0,5л",
-    image:
-      "https://i0.wp.com/samogonman.com/wp-content/uploads/2018/01/3633.jpg",
+    image: beerCup,
     description:
       "Світле нефільтроване непастеризоване пиво «Light beer», світло-солом’яного кольору, має слабкий солодовий смак з легкою хмелевою гіркуватістю та приємними фруктовими нотами.",
     price: 49,
@@ -82,8 +82,7 @@ export const beer = [
     alcohol: 5.0,
     ibu: "",
     weight: "0,5л",
-    image:
-      "https://divinepizza.com.ua/image/cache/catalog/%D0%9D%D0%B0%D0%BF%D0%B8%D1%82%D0%BA%D0%B8/63c0005e5b_500-500x500.jpg",
+    image: seedr,
     description: "Сідр яблучний",
     price: 50,
   },
@@ -96,8 +95,7 @@ export const beer = [
     alcohol: 10.5,
     ibu: "",
     weight: "0,5л",
-    image:
-      "https://www.wineenthusiast.com/wp-content/uploads/2023/08/08_23_Everything_You_Need_to_Know_About_Prosecco_HERO_GettyImages-1185246504_1920x1280.jpg",
+    image: prosecco,
     description: "Prosecco",
     price: 70,
   },
@@ -110,8 +108,7 @@ export const beer = [
     alcohol: "",
     ibu: "",
     weight: "0,5л",
-    image:
-      "https://calorizator.ru/sites/default/files/imagecache/product_512/product/kvass.jpg",
+    image: kvas,
     description: "Квас",
     price: 30,
   },
