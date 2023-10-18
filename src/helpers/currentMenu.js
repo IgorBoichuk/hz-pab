e3c57f6bff752598adcd36b34d33a30ef85c9067
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-10-18 16:10:15 +0300

    some bugs

--- src/helpers/currentMenu.js
+++ src/helpers/currentMenu.js
@@ -2,27 +2,26 @@ import { beer } from "../assets/data/beerData";
 import { menu } from "../assets/data/menuData";
 import { snacks } from "../assets/data/snacksData";
 
-export const uniqCat = (data) => {
-  const datacategoryes = data.map((item) => item.subCategory);
-  return [...new Set(datacategoryes)];
+export const uniqCat = data => {
+	const datacategoryes = data.map(item => item.subCategory);
+	return [...new Set(datacategoryes)];
 };
 
-export const currentMenu = (data) => {
-  switch (data) {
-    case "bar":
-      return uniqCat(beer);
+export const currentMenu = data => {
+	switch (data) {
+		case "bar":
+			return uniqCat(beer);
 
-    case "kitchen":
-      return uniqCat(menu);
+		case "kitchen":
+			return uniqCat(menu);
 
-    case "snacks":
-      return uniqCat(snacks);
+		case "snacks":
+			return uniqCat(snacks);
 
-    case "souses":
-      // return uniqCat(souses);
-      return null;
+		case "souses":
+			return null;
 
-    default:
-      return null;
-  }
+		default:
+			return null;
+	}
 };
--- src/modules/Home/AboutUs/AboutUs.jsx
+++ src/modules/Home/AboutUs/AboutUs.jsx
@@ -6,49 +6,42 @@ import { SectionTitle } from "../../shared/SectionTitle/SectionTitle";
 import sprite from "../../../assets/galary/svg/sprite.svg";
 
 export const AboutUs = ({ title }) => {
-  return (
-    <section className={style.section}>
-      <Container>
-        <div className={style.sectionWrapper}>
-          <div className={style.barImgWrapper}>
-            <div className={style.linkwrapper}>
-              <SectionTitle title={title} />
-              <Link to="about-us" className={style.link}>
-                <p className={style.linkText}>
-                  Ласкаво просимо у "ХЗ паб" – найкращий пивний паб в ЖК
-                  Петрівський Квартал. Тут панує справжня дружня атмосфера де ви
-                  зможете розслабитися та забути про повсякденність.
-                </p>
-                <p className={style.linkPath}>детальніше → </p>
-              </Link>
-              {/* <img loading='lazy' src={beer} alt="beer" className={style.beerImg} /> */}
-              <div className={style.beerImgWrapper}>
-                <div className={style.beerImg}></div>
-                <div className={style.barImgMob}></div>
-              </div>
-            </div>
-
-            <div className={style.barImg}>
-              {/* <img loading='lazy' src={bar} alt="bar" className={style.barImg} /> */}
-            </div>
-          </div>
-          <div className={style.rightPartWrapper}>
-            {/* <img loading='lazy' src={beerTap} alt="beerTap" className={style.beerTapImg} /> */}
-            <div className={style.beerTapImg}></div>
-            <div className={style.promoWrapper}>
-              <div className={style.quoteWrapper}>
-                <svg className={style.quote} width="40" height="40">
-                  <use xlinkHref={`${sprite}#quote`}></use>
-                </svg>
-              </div>
-              <p className={style.promoText}>
-                "ХЗ паб" – це місце, де час зупиняється, а веселі зустрічі з
-                друзями стають ще приємнішими
-              </p>
-            </div>
-          </div>
-        </div>
-      </Container>
-    </section>
-  );
+	return (
+		<section className={style.section}>
+			<Container>
+				<div className={style.sectionWrapper}>
+					<div className={style.barImgWrapper}>
+						<div className={style.linkwrapper}>
+							<SectionTitle title={title} />
+							<Link to="about-us" className={style.link}>
+								<p className={style.linkText}>
+									Ласкаво просимо у "ХЗ паб" – найкращий пивний паб в ЖК Петрівський Квартал. Тут панує справжня дружня
+									атмосфера де ви зможете розслабитися та забути про повсякденність.
+								</p>
+								<p className={style.linkPath}>детальніше → </p>
+							</Link>
+							<div className={style.beerImgWrapper}>
+								<div className={style.beerImg}></div>
+								<div className={style.barImgMob}></div>
+							</div>
+						</div>
+						<div className={style.barImg}></div>
+					</div>
+					<div className={style.rightPartWrapper}>
+						<div className={style.beerTapImg}></div>
+						<div className={style.promoWrapper}>
+							<div className={style.quoteWrapper}>
+								<svg className={style.quote} width="40" height="40">
+									<use xlinkHref={`${sprite}#quote`}></use>
+								</svg>
+							</div>
+							<p className={style.promoText}>
+								"ХЗ паб" – це місце, де час зупиняється, а веселі зустрічі з друзями стають ще приємнішими
+							</p>
+						</div>
+					</div>
+				</div>
+			</Container>
+		</section>
+	);
 };
