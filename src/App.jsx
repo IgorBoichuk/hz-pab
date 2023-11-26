e89b15c2942eba7233ba6019a954cab23a4ce395
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-11-26 22:13:02 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import { Route, Routes } from "react-router-dom";
-import React, { lazy } from "react";
+import { Route, Routes } from 'react-router-dom';
+import React, { lazy } from 'react';
 
-import { BeerPreview } from "./modules/Menu/BeerPreview/BeerPreview";
-import { SharedLayout } from "./components/SharedLayout/SharedLayout";
+// import { BeerPreview } from './modules/Menu/BeerPreview/BeerPreview';
+import { SharedLayout } from './components/SharedLayout/SharedLayout';
 
-import { Home } from "./pages/Home";
+import { Home } from './pages/Home';
+import { MenuContent } from './modules/Menu';
 
-const About = lazy(() => import("./pages/About"));
-const Menu = lazy(() => import("./pages/Menu"));
-const Atmosphere = lazy(() => import("./pages/Atmosphere"));
-const Contacts = lazy(() => import("./pages/Contacts"));
-const ErrorPage = lazy(() => import("./pages/ErrorPage"));
+const About = lazy(() => import('./pages/About'));
+const Menu = lazy(() => import('./pages/Menu'));
+const Atmosphere = lazy(() => import('./pages/Atmosphere'));
+const Contacts = lazy(() => import('./pages/Contacts'));
+const ErrorPage = lazy(() => import('./pages/ErrorPage'));
 
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<SharedLayout />}>
+			<Route path='/' element={<SharedLayout />}>
 				<Route index element={<Home />} />
-				<Route path="/about-us" element={<About />} />
-				<Route path="/menu" element={<Menu />}>
-					<Route path="bar" element={<BeerPreview />} />
+				<Route path='/about-us' element={<About />} />
+				<Route path='/menu' element={<Menu />}>
+					<Route path='/menu/:id' element={<MenuContent />} />
+					{/* <Route path="bar" element={<BeerPreview />} />
 					<Route path="snacks" element={<BeerPreview />} />
 					<Route path="kitchen" element={<BeerPreview />} />
-					<Route path="souses" element={<BeerPreview />} />
+					<Route path="souses" element={<BeerPreview />} /> */}
 				</Route>
-				<Route path="/atmosphere" element={<Atmosphere />} />
-				<Route path="/contacts" element={<Contacts />} />
-				<Route path="*" element={<ErrorPage />} />
+				<Route path='/atmosphere' element={<Atmosphere />} />
+				<Route path='/contacts' element={<Contacts />} />
+				<Route path='*' element={<ErrorPage />} />
 			</Route>
 		</Routes>
 	);
--- src/Context/ContextProvider.jsx
+++ src/Context/ContextProvider.jsx
@@ -1,52 +1,50 @@
-import React, { createContext, useCallback, useEffect, useState } from "react";
-import { useLocation, useSearchParams } from "react-router-dom";
-import { allMenuData, normalizeSubMenu, uniqCat } from "../helpers";
+import React, { createContext, useCallback, useEffect, useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import { allMenuData, normalizeSubMenu, uniqCat } from '../helpers';
 export const MyContext = createContext();
 
 export const ContextProvider = ({ children }) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const subMenu = searchParams.get("SubMenu");
+	const [searchParams, setSearchParams] = useSearchParams();
+	const subMenu = searchParams.get('SubMenu');
 
-  const [filteredData, setFilteredData] = useState([]);
-  const location = useLocation();
-  const active = location.pathname.split("/");
-  const activeSubMenu = active[active.length - 1];
+	const [filteredData, setFilteredData] = useState([]);
+	const location = useLocation();
+	const active = location.pathname.split('/');
+	const activeSubMenu = active[active.length - 1];
 
-  const submenu = uniqCat(allMenuData);
+	const submenu = uniqCat(allMenuData);
 
-  const filterByCategory = useCallback(() => {
-    let activeItem = normalizeSubMenu(activeSubMenu);
+	const filterByCategory = useCallback(() => {
+		let activeItem = normalizeSubMenu(activeSubMenu);
 
-    const filteredArray = allMenuData.filter((item) => {
-      if (subMenu) {
-        return item.subCategory === subMenu;
-      }
-      return item.category === activeItem;
-    });
-    setFilteredData(filteredArray);
-  }, [activeSubMenu, subMenu]);
+		const filteredArray = allMenuData.filter(item => {
+			if (subMenu) {
+				return item.subCategory === subMenu;
+			}
+			return item.category === activeItem;
+		});
+		setFilteredData(filteredArray);
+	}, [activeSubMenu, subMenu]);
 
-  useEffect(() => {
-    filterByCategory();
-  }, [filterByCategory]);
+	useEffect(() => {
+		filterByCategory();
+	}, [filterByCategory]);
 
-  const preview = subMenu || normalizeSubMenu(activeSubMenu);
+	const preview = subMenu || normalizeSubMenu(activeSubMenu);
 
-  const handleSetSearchParams = (item) => {
-    setSearchParams({ SubMenu: item });
-    filterByCategory();
-  };
+	const handleSetSearchParams = item => {
+		setSearchParams({ SubMenu: item });
+		filterByCategory();
+	};
 
-  const contextValue = {
-    activeSubMenu,
-    handleSetSearchParams,
-    filteredData,
-    searchParams,
-    preview,
-    submenu,
-  };
+	const contextValue = {
+		activeSubMenu,
+		handleSetSearchParams,
+		filteredData,
+		searchParams,
+		preview,
+		submenu,
+	};
 
-  return (
-    <MyContext.Provider value={contextValue}>{children}</MyContext.Provider>
-  );
+	return <MyContext.Provider value={contextValue}>{children}</MyContext.Provider>;
 };
